bb0043d32b3c6cca82e1274d001759a6e9f5efb6
Tyuiu.BrukhovAA@outlook.com <bruhov_uch@mail.ru>
2023-10-31 20:22:35 +0500

    Add project files.

---
+++ Tyuiu.BrukhovAA.Sprint4.Task0.V3.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.BrukhovAA.Sprint4.Task0.V3.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.BrukhovAA.Sprint4.Task0.V3.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7ff42b88-63ab-4d1c-8620-bc4686095cf2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
